35efd59a4cb0aa67a5b1ca6c5fb373b796ac85dc
Thisma10 <math.cutullic@gmail.com>
2021-06-11 12:46:24 +0200

    petite structuration du code

--- jeu.js
+++ jeu.js
@@ -4,7 +4,7 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
@@ -29,8 +29,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         if (dice !== 1) {
             //Add score
             roundScore += dice;
-            /*document.querySelector('#current-' + activePlayer).textContent = roundScore;*/
-            document.querySelector('${#name}-${activePlayer}').textContent = roundScore;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
             //Next player
             nextPlayer();
@@ -46,6 +45,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
         // Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+       
 
         // Check if player won the game
         if (scores[activePlayer] >= 100) {
@@ -73,9 +73,6 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    //document.querySelector('.player-0-panel').classList.remove('active');
-    //document.querySelector('.player-1-panel').classList.add('active');
-
     document.querySelector('.de').style.display = 'none';
 }
 
@@ -101,7 +98,3 @@ function init() {
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
 }
-
-//document.querySelector('#current-' + activePlayer).textContent = de;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + de + '</em>';
-//let x = document.querySelector('#score-0').textContent;
